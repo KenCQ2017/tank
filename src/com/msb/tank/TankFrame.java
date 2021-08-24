395b5b207e144eef541ce4e995d8cd07a0cafcf1
Ken <2594717524@qq.com>
2021-08-25 05:39:28 +0800

    处理闪烁

--- src/com/msb/tank/TankFrame.java
+++ src/com/msb/tank/TankFrame.java
@@ -7,11 +7,13 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class TankFrame extends Frame {
+    static final int GAME_WIDTH = 800, GAME_HEIGHT = 600;
+
     Tank myTank = new Tank(200,200,Dir.DOWN);
     Bullet b = new Bullet(300,300,Dir.DOWN);
 
     public TankFrame() {
-        setSize(800,600);
+        setSize(GAME_WIDTH,GAME_HEIGHT);
         setResizable(false);
         setTitle("Tank War");
         setVisible(true);
@@ -24,6 +26,22 @@ public class TankFrame extends Frame {
                 System.exit(0);
             }
         });
+    }
+
+    //在内存定义一张图片，先画到内存，再一次性画到屏幕上【把内存内容复制到显存】，防止闪烁
+    Image offScreenImage = null;
+    @Override
+    public void update(Graphics g) { //g时屏幕上的画笔
+        if (offScreenImage == null) {
+            offScreenImage = this.createImage(GAME_WIDTH, GAME_HEIGHT);
+        }
+        Graphics gOffScreen = offScreenImage.getGraphics();
+        Color c = gOffScreen.getColor();
+        gOffScreen.setColor(Color.BLACK);
+        gOffScreen.fillRect(0,0,GAME_WIDTH,GAME_HEIGHT);
+        gOffScreen.setColor(c);
+        paint(gOffScreen);
+        g.drawImage(offScreenImage,0,0,null);
     }
 
     @Override
