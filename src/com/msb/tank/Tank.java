904f12791c81a4aa7c67fc1970d4a5d767296636
Ken <2594717524@qq.com>
2021-08-25 05:57:40 +0800

    让Tank类持有TankFrame类对象的引用，ctrl键发出一颗子弹

--- src/com/msb/tank/Tank.java
+++ src/com/msb/tank/Tank.java
@@ -7,11 +7,13 @@ public class Tank {
     private Dir dir = Dir.DOWN;
     private static final int SPEED = 5;
     private boolean moving = false;
+    private TankFrame tf = null;        //持有TankFrame对象的引用
 
-    public Tank(int x, int y, Dir dir) {
+    public Tank(int x, int y, Dir dir, TankFrame tf) {
         this.x = x;
         this.y = y;
         this.dir = dir;
+        this.tf  = tf;
     }
 
     public int getX() {
@@ -67,5 +69,9 @@ public class Tank {
                 y += SPEED;
                 break;
         }
+    }
+
+    public void fire() {
+        tf.b = new Bullet(this.x,this.y,this.dir);
     }
 }
