f93db7fe2770326f089047b34013103abbebcd97
Ken <2594717524@qq.com>
2021-08-25 06:18:18 +0800

    支持一连串子弹的发射，消除子弹迭代器由于子弹没有释放最终导致的内存泄露问题

--- src/com/msb/tank/Bullet.java
+++ src/com/msb/tank/Bullet.java
@@ -3,18 +3,25 @@ package com.msb.tank;
 import java.awt.*;
 
 public class Bullet {
-    private static final int SPEED = 8;
+    private static final int SPEED = 15;
     private static int WIDTH = 25, HEIGHT = 25;
     private int x, y;
     private Dir dir;
+    private TankFrame tf = null;
+    private boolean live = true;
 
-    public Bullet(int x, int y, Dir dir) {
+    public Bullet(int x, int y, Dir dir, TankFrame tf) {
         this.x = x;
         this.y = y;
         this.dir = dir;
+        this.tf = tf;
     }
 
     public void paint(Graphics g) {
+        if(!live) {
+            tf.bullets.remove(this);
+        }
+
         Color c = g.getColor();
         g.setColor(Color.RED);
         g.fillOval(x,y,WIDTH,HEIGHT);
@@ -36,6 +43,9 @@ public class Bullet {
             case DOWN:
                 y += SPEED;
                 break;
+        }
+        if (x < 0 || y <0 || x > TankFrame.GAME_WIDTH || y > TankFrame.GAME_HEIGHT) {
+            this.live = false;
         }
     }
 }
--- src/com/msb/tank/TankFrame.java
+++ src/com/msb/tank/TankFrame.java
@@ -5,12 +5,15 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TankFrame extends Frame {
     static final int GAME_WIDTH = 800, GAME_HEIGHT = 600;
-
+    List<Bullet> bullets = new ArrayList<>();
     Tank myTank = new Tank(200,200,Dir.DOWN, this);
-    Bullet b = new Bullet(300,300,Dir.DOWN);
+
+//    Bullet b = new Bullet(300,300,Dir.DOWN);
 
     public TankFrame() {
         setSize(GAME_WIDTH,GAME_HEIGHT);
@@ -46,8 +49,15 @@ public class TankFrame extends Frame {
 
     @Override
     public void paint(Graphics g) {
+        Color c = g.getColor();
+        g.setColor(Color.WHITE);
+        g.drawString("子弹的数量：" + bullets.size(), 10, 60);
+        g.setColor(c);
+
         myTank.paint(g);    //把画笔传递给tank类
-        b.paint(g);
+        for(int i = 0; i < bullets.size(); i++) {
+            bullets.get(i).paint(g);
+        }
     }
 
     class MyKeyListener extends KeyAdapter {
